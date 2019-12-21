ddf675fa1f386bca023e1329a1bc0b8da3d189b6
lj-fh <lj-fh@189china.net>
2019-12-21 19:09:10 +0800

    init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# 小说爬虫
+
+1. 推荐使用 https://www.bodekang.com/ 该笔趣阁小说爬虫
+2. 借鉴项目：https://blog.csdn.net/jdbdh/article/details/90673758，感谢博主
+3. 样式选择器写法：选中标题，右键检查，element中选中该区域右键copy-->copy selector.图解参考借鉴博客中内容
---
+++ src/main/java/SpriderRun.java
@@ -0,0 +1,29 @@
+/**
+ * @author 方华
+ * @date 2019-12-21
+ */
+public class SpriderRun {
+
+    public static void main(String... args) {
+        try {
+
+        //开启爬虫
+        Utils utils = new Utils("https://www.bodekang.com/86_86588/25246169.html",
+                "#main > div > div > div.bookname > h1",
+                "#content",
+                "#main > div > div > div.bottem2 > a.next",
+                "人世浮沉.txt");
+        utils.crawl();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    void getHtmlMes(){
+        Utils utils = new Utils("https://www.bodekang.com/86_86588/25246169.html",
+                "#main > div > div > div.bookname > h1",
+                "#content",
+                "#main > div > div > div.bottem2 > a.next",
+                "人世浮沉.txt");
+    }
+
+}
